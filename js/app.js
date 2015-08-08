a5e3cbce4146e5e379b52306824eaa4bd48e3b4d
Brian Neaves <brian.g.neaves@gmail.com>
2015-08-08 12:28:25 -0400

    Added comments

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    //Sets a random start place for each Enemy so that they are spaced evenly
     this.x = Math.random()*2000 - 1500;
+    //Sets a random path for the Enemy so that the enemies can travel side by side
     this.y = 40 + Math.random()*200;
     this.speed = Math.random()*100;
 
@@ -19,24 +21,21 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = this.x + this.speed * dt;
+    //Resets the Player's position in the event of a collision
     if(Math.abs(player.x - this.x) < 30 && Math.abs(player.y - this.y < 30)){
         player.reset();
     }
 }
-
-
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-var playerObject = function(){
+// Hero sprite that the player controls
+var playerObject = function() {
     this.hero = 'images/char-boy.png';
+    //Starting place for hero sprite
     this.x = 205;
     this.y = 405;
 }
@@ -46,38 +45,44 @@ playerObject.prototype.update = function(){
 
 }
 
+//Draw the hero sprite on the board
 playerObject.prototype.render = function() {
     ctx.drawImage(Resources.get(this.hero), this.x, this.y);
 }
 
-playerObject.prototype.reset = function(){
+
+//Sends the hero sprite back to the start place, resetting the game
+playerObject.prototype.reset = function() {
     this.x = 205;
     this.y = 405;
 }
 
-playerObject.prototype.handleInput= function(allowedKeys){
-    if(allowedKeys == 'left'){
+playerObject.prototype.handleInput= function(allowedKeys) {
+    //If the Left arrow key is pressed, move the hero sprite to the next tile to the left
+    if(allowedKeys == 'left') {
         this.x = this.x - 101;
-        if(this.x < 2){
+        //But not off the board
+        if(this.x < 2) {
             this.x = this.x + 101;
         }
     }
-    if(allowedKeys == 'right'){
+    if(allowedKeys == 'right') {
         this.x = this.x + 101;
-        if(this.x > 505){
+        if(this.x > 505) {
             this.x = this.x - 101;
         }
     }
-    if(allowedKeys == 'down'){
+    if(allowedKeys == 'down') {
         this.y = this.y + 83;
-        if(this.y > 475){
+        if(this.y > 475) {
             this.y = this.y - 83;
         }
     }
-    if(allowedKeys == 'up'){
+    if(allowedKeys == 'up') {
         this.y = this.y - 83;
     }
-    if(this.y < 50){
+    //If the hero sprite gets to the water, reset the game and announce that the player has won.
+    if(this.y < 50) { 
         this.reset();
         alert("You won!");
     }
@@ -88,8 +93,8 @@ playerObject.prototype.handleInput= function(allowedKeys){
 // Place the player object in a variable called player
 var allEnemies = [];
 
-
-for(var i = 0; i < 50; i++){
+//Create 50 enemies
+for(var i = 0; i < 50; i++) { 
     allEnemies.push(new Enemy());
 }
 
